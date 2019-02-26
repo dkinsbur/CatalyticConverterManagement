2f37c0336cfad67825074bd5ad9d3f4c202924f3
Kinsbursky <daniel.kinsbursky@intel.com>
2019-02-27 00:51:49 +0200

    quick and dirty cash register

--- CatalyticConverterManagement/PurchacePage.xaml.cs
+++ CatalyticConverterManagement/PurchacePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -31,6 +32,116 @@ namespace CatalyticConverterManagement
 
             DataContext = new PurchacePageViewModel(db.GetConverters());
 
+            PurchaseList = new ObservableCollection<PurchaseEntry>();
+            lstPurchase.ItemsSource = PurchaseList;
+            tbSearch.Focus();
+
+        }
+
+        public ObservableCollection<PurchaseEntry> PurchaseList;
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            (DataContext as PurchacePageViewModel).UpdateFilter((sender as TextBox).Text);
+            list.SelectedIndex = 0;
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                if (waitForEnterRelease)
+                {
+                    waitForEnterRelease = false;
+                }
+                else
+                {
+                    tbWhole.Focus();
+                }
+            }
+            else if (e.Key == Key.Up)
+            {
+                list.SelectedIndex -= 1;
+            }
+            else if (e.Key == Key.Down)
+            {
+                list.SelectedIndex += 1;
+            }
+            else if (e.Key == Key.Down)
+            {
+                list.SelectedIndex += 1;
+            }
+
+
+
+        }
+
+        private void tbWhole_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                tbHalf.Focus();
+            }
+        }
+
+        private void tbHalf_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                btnAdd.Focus();
+            }
+        }
+
+        private void list_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                tbWhole.Focus();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var conv = (ctrlConv.DataContext as Converter);
+            if (conv == null)
+            {
+                return;
+            }
+
+            int whole = 0;
+            int half = 0;
+            var txtWhole = tbWhole.Text.Trim();
+            var txtHalf = tbHalf.Text.Trim();
+            if (txtWhole != "")
+            {
+                if(!int.TryParse(txtWhole, out whole))
+                {
+                    MessageBox.Show(string.Format("Bad Whole Value '{0}' ", txtWhole), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            if (txtHalf != "")
+            {
+                if (!int.TryParse(txtHalf, out half))
+                {
+                    MessageBox.Show(string.Format("Bad Whole Value '{0}' ", txtHalf), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            PurchaseList.Add(new PurchaseEntry() { Converter = conv, ManualPriceSet = false, Count = whole, HalfCount = half });
+            tbSearch.Text = "";
+            tbWhole.Text = "";
+            tbHalf.Text = "";
+            tbSearch.Focus();
+        }
+
+        bool waitForEnterRelease = false;
+
+        private void tbSearch_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(Keyboard.IsKeyDown( Key.Enter))
+            {
+                waitForEnterRelease = true;
+            }
         }
     }
 
@@ -48,11 +159,33 @@ namespace CatalyticConverterManagement
             }
         }
 
+        public void UpdateFilter(string filter)
+        {
+            _filter = filter;
+            _converters.Refresh();
+        }
+        string _filter="";
+
         public PurchacePageViewModel(List<Converter> converters)
         {
             _converters = CollectionViewSource.GetDefaultView(converters);
+            _converters.Filter = ConverterListFilter;
         }
 
+        bool ConverterListFilter(object item)
+        {
+            var conv = (item as Converter);
+            var filtArgs = _filter.Trim().ToUpper().Split(null);
+            foreach (var filt in filtArgs)
+            {
+                if (!conv.FullName.ToUpper().Contains(filt))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
         public ICollectionView Converters { get { return _converters; } }
     }
 }
